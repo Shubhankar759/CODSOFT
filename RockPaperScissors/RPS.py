f44312d3ae3a609f83dd29be3b1b53347518fb65
Shubhankar759 <shubhankarwarkade@gmail.com>
2024-01-27 13:45:12 +0530

    final touches
    added images completed main menu remove some overlapping in buttons tested all possible rounds  also added comments here starts play and improve state

--- RockPaperScissors/RPS.py
+++ RockPaperScissors/RPS.py
@@ -1,20 +1,29 @@
+# pygame and random module imported
 import pygame
 import random
 
+#Window icon and name 
 pygame.init()
 screen = pygame.display.set_mode((400,500))
 pygame.display.set_caption("Rock Paper Scissors")
+Game_icon=pygame.image.load('Game_logo.png')
+pygame.display.set_icon(Game_icon)
 
-
-count_round=1
+#all variables to handle user/computer inputs, number of rounds and result  
+round=0
+count_round=0
 choice=0
+comp_choice=0
 score_your=0
 score_comp=0
 result=0
 
+#all fonts used in game
 font = pygame.font.SysFont('Roboto',40,bold=True)
 font_1=pygame.font.SysFont('Roboto',25,bold=True)
+font_2=pygame.font.SysFont('Roboto',45,bold=True)
 
+#all text surfaces used in game
 Quit = font.render('Quit',True,'white')
 Start = font.render('Start',True,'white')
 Round_1 = font.render('5 Rounds',True,'white')
@@ -25,17 +34,16 @@ Next = font.render('Next',True,'white')
 Retry = font.render('Retry',True,'white')
 Exit = font.render('Exit',True,'white')
 
-
-
-
+#all images imported
 Rock = pygame.image.load('Rock.png')
 Rock = pygame.transform.scale(Rock,(100,100))
 Paper = pygame.image.load('Paper.png')
 Paper = pygame.transform.scale(Paper,(100,100))
 Scissors = pygame.image.load('Scissors.png')
 Scissors = pygame.transform.scale(Scissors,(100,100))
+Game_icon_ingame = pygame.transform.scale(Game_icon,(200,200))
 
-
+#all buttons for user interaction
 Quit_button =  pygame.Rect(250,350,68,30)
 Start_button = pygame.Rect(90,350,68,30)
 Round_button_1 = pygame.Rect(135,150,130,30)
@@ -49,26 +57,31 @@ Next_button = pygame.Rect(173,410,60,30)
 Retry_button=pygame.Rect(70,410,70,30)
 Exit_button = pygame.Rect(260,410,60,30)
 
-
-
+#all states of application
 run =True
 Menu_state=True
 Rounds_state=False
 Game_state=False
-still_game_state=True
+still_game_state=False
 Final_state=False
 
+#collection of surfaces and function to display on current screen
 def Start_menu():
      pygame.draw.rect(screen,('black'),Quit_button)
      pygame.draw.rect(screen,('black'),Start_button)
     
-    
      screen.blit(Quit, (Quit_button.x,Quit_button.y))
      screen.blit(Start,(Start_button.x,Start_button.y))
+   
+     Title=font_2.render("ROCK PAPER SCISSORS",True,'white')
+     screen.blit(Title,(12,30))
+     
+     screen.blit(Game_icon_ingame,(100,100))
 
 def Round_menu():
     
      text_1 = font.render("Choose number of rounds",True,"white")
+     
      pygame.draw.rect(screen,('black'),Round_button_1)
      screen.blit(Round_1,(Round_button_1.x,Round_button_1.y))
      
@@ -95,8 +108,8 @@ def Game():
     screen.blit(Scissors,Scissors_button)
     
     
-    text_3 = font.render(f"Round {count_round}",True,'white')
-    screen.blit(text_3,(130,10))
+    text_3 = font.render(f"Round {count_round+1}",True,'white')
+    if count_round<round:screen.blit(text_3,(130,10))
     
     if  still_game_state and count_round<=round:
      text_2 = font_1.render("Choose your move",True,"white")
@@ -114,8 +127,17 @@ def Game():
         pygame.draw.rect(screen,('black'),Exit_button)
         screen.blit(Exit,(Exit_button.x,Exit_button.y))
         
-
- 
+        if score_your>score_comp:
+            won = font.render('You WON',True,'green')
+            screen.blit(won,(140,100))
+        elif score_your<score_comp:
+            lost= font.render('You LOST',True,'red')
+            screen.blit(lost,(140,100))
+        else:
+            tie = font.render("DRAW",True,'blue')
+            screen.blit(tie,(160,100))
+        
+#all random inputs of computer
 def Comp_bot():
      global result
      if comp_choice==1:screen.blit(Rock,(140,60))
@@ -128,24 +150,33 @@ def Comp_bot():
          result=1
      if choice==comp_choice:
          result=0
-        
+
+# main loop      
 while run:
     
-    screen.fill("red")
+    screen.fill("black")#background color
     
+    #all states fitted into loop
     if Menu_state:    
          Start_menu()
     if Rounds_state:
         Round_menu()
     if Game_state:
         Game()
-        Score=font.render(f"{score_your}",True,'white')
-        screen.blit(Score,(10,10))
+        Score_u=font.render(f"{score_your}",True,'green')
+        screen.blit(Score_u,(170,190))
         
+        seprt=font.render(":",True,'white')
+        screen.blit(seprt,(193,190))
         
+        Score_comp=font.render(f"{score_comp}",True,'red')
+        screen.blit(Score_comp,(210,190))
+    
+    #for loop responsible for all action taken by user  
     for events in pygame.event.get():
         if events.type == pygame.QUIT:
             run= False
+        #all mouse inputs are collected and processed accordingly
         if events.type == pygame.MOUSEBUTTONDOWN:
             if  Quit_button.collidepoint(events.pos)and Menu_state==True:
                 run= False 
@@ -154,26 +185,6 @@ while run:
                Menu_state=False
                Rounds_state=True
                
-            if Round_button_1.collidepoint(events.pos) and Rounds_state==True:
-                round=5
-                Rounds_state=False
-                Game_state=True
-                
-            if Round_button_2.collidepoint(events.pos)and Rounds_state==True:
-                round=10
-                Rounds_state=False
-                Game_state=True
-                
-            if Round_button_3.collidepoint(events.pos)and Rounds_state==True:
-                round=15
-                Rounds_state=False
-                Game_state=True
-                
-            if Round_button_4.collidepoint(events.pos)and Rounds_state==True:
-                round=20
-                Rounds_state=False
-                Game_state=True
-                
             if Rock_button.collidepoint(events.pos) and Game_state==True and still_game_state==True:
                 choice=1
                 still_game_state=False
@@ -186,6 +197,30 @@ while run:
                 choice=3
                 still_game_state=False
                 comp_choice= random.randint(1,3)
+               
+            if Round_button_1.collidepoint(events.pos) and Rounds_state==True:
+                round=5
+                Rounds_state=False
+                Game_state=True
+                still_game_state=True
+                
+            if Round_button_2.collidepoint(events.pos)and Rounds_state==True:
+                round=10
+                Rounds_state=False
+                Game_state=True
+                still_game_state=True
+                
+            if Round_button_3.collidepoint(events.pos)and Rounds_state==True:
+                round=15
+                Rounds_state=False
+                Game_state=True
+                still_game_state=True
+                
+            if Round_button_4.collidepoint(events.pos)and Rounds_state==True:
+                round=20
+                Rounds_state=False
+                Game_state=True
+                still_game_state=True
                 
             if Next_button.collidepoint(events.pos):
                if count_round<round:
@@ -197,13 +232,10 @@ while run:
                if result==1:score_your+=1
                if result==-1:score_comp+=1
                     
-                   
-                
-            
             if Retry_button.collidepoint(events.pos) and Final_state==True:
-                count_round=1
+                count_round=0
                 still_game_state=True
-                score_you=0
+                score_your=0
                 score_comp=0
             
             if Exit_button.collidepoint(events.pos) and Final_state==True:
@@ -213,10 +245,8 @@ while run:
                 Menu_state=True
                 count_round=1
      
-
+    #updating screen after every single input
     pygame.display.update()
-    
-still_game_state=True
-   
 
+#end of application
 pygame.quit()
